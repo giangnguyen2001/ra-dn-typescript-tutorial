4fd38c1cbc77ae2314487380f2908102c9355993
giangnguyen2001 <nguyentruonggiang131201@gmail.com>
2023-09-27 21:19:25 +0700

    session 05 - interface types

--- index.ts
+++ index.ts
@@ -1,5 +1,33 @@
-import { MomoQRPayment } from "./models/MomoQRPayment";
+import { Order } from "./types/Order";
+import { Person } from "./types/Person";
 
-const momoQrPayment: MomoQRPayment = new MomoQRPayment(234, '0123456789');
-console.log('momoQrPayment', momoQrPayment);
-momoQrPayment.pay();
+// Not good
+const sang: { first_name: string; last_name: string; age: number } = {
+  first_name: "Sang",
+  last_name: "Quang",
+  age: 23,
+};
+
+const thai: Person = {
+  first_name: "Thai",
+  last_name: "Le Quang",
+  age: 25,
+};
+const son: Person = {
+  first_name: "Son",
+  last_name: "Le Hoang",
+  age: 24,
+};
+
+const order: Order = {
+  order_id: 1,
+  order_at: new Date(),
+  user_id: 1,
+  note: "",
+  order_details: [],
+};
+
+console.log("sang", sang);
+console.log("thai", thai);
+console.log("son", son);
+console.log("order", order);
